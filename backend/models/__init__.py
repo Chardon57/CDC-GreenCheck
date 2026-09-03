22942176c1cc5e9b1fcd83941cd07c2dd2c07ad2
Jean-Patrick Glad <jpglad@protonmail.com>
2026-09-03 16:00:08 +0200

    Ajout de models/__init__.py pour la centralisation des déclaration des modèles

---
+++ backend/models/__init__.py
@@ -0,0 +1,8 @@
+from .user import User
+from .plant import Plant
+from .result import Result
+from .treatment import Treatment
+from .analysis import Analysis
+from .associations import impliquer
+
+__all__ = ["User", "Plant", "Result", "Treatment", "Analysis", "impliquer"]
