1e60f3ee45c702c6f79768ba9f3e8a4d4af0da50
Jean-Patrick Glad <jpglad@protonmail.com>
2026-09-04 16:09:21 +0200

    Màj mineure : correction de fautes de frappe

--- backend/schemas/treatment.py
+++ backend/schemas/treatment.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel, ConfigDict
 class TreatmentRead(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
-    id_treatmentt: int
+    id_treatment: int
     advice: str
