49c7aec042f2a3ee0d4441188ff88ef2ca4f9609
Jean-Patrick Glad <jpglad@protonmail.com>
2026-09-04 08:55:03 +0200

    Mise à jour de dev avant merge vers main

--- initialise_db_and_user.sql
+++ initialise_db_and_user.sql
@@ -1,11 +1,11 @@
 -- Création de la base de donnée nécessaire
-CREATE DATABASE IF NOT EXISTS greencheck_db;
+CREATE DATABASE IF NOT EXISTS greencheck;
 
 -- Création de l'utilisateur greencheck
 CREATE USER 'greencheck_user'@'%' IDENTIFIED BY 'greencheck_pass';
 
 -- Ajout des droits pour cet utilisateur
-GRANT ALL PRIVILEGES ON greencheck_db.* TO 'greencheck_user'@'%';
+GRANT ALL PRIVILEGES ON greencheck.* TO 'greencheck_user'@'%';
 
 -- Application des changements
 FLUSH PRIVILEGES;
